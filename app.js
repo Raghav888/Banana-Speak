1f6d10286ece867caababef6665842a8e3b6a17e
Raghav Bang <raghavbang2012@gmail.com>
2022-01-15 14:50:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,20 +2,19 @@ const btnTranslate = document.querySelector("#btn-tran")
 const data = document.querySelector("#inputdata")
 const out = document.querySelector("#output")
 const img_out = document.querySelector("#img")
-var url = "https://api.funtranslations.com/translate/minion.json?text="
+const url = "https://api.funtranslations.com/translate/minion.json?text="
 
-function errorHandler(error) {
-    console.log(error)
-}
+const errorHandler=(error)=>console.log(error);
+
 
 //milliseconds until timeout//
-function hideElement() {
+const hideElement=()=> {
     document.getElementById("img_o").src = ""
 }
 
-function messgae() {
+const messgae=()=> {
     url = url + data.value
-    var data_out = fetch(url).then(response => response.json()).
+    let data_out = fetch(url).then(response => response.json()).
     then(json => { out.innerText = json.contents.translated }).catch(errorHandler)
 
     document.getElementById("img_o").src = "thumbsup.gif"
